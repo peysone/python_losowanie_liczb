bcd5588c2b377438df872b9711a7297574edca8d
Peysone <peysone@MacBook-Pro-Peysone.local>
2023-04-23 18:46:22 +0200

    dodanie programu do losowania liczb

---
+++ py1project_losowanie.py
@@ -0,0 +1,12 @@
+a = 10
+b = 3
+
+print(f'Liczby: [{a}] [{b}]')
+print('Dzielenie [true division]: ', a / b)
+print('Mnożenie', a * b)
+print('Dodawanie', a + b)
+print('Odejmowanie', a - b)
+print('Dzielenie bez reszty [floor division] ', a // b)
+print('Reszta z dzielenia [mod]: ', a % b)
+print('Potęgowanie:', a ** b)
+
